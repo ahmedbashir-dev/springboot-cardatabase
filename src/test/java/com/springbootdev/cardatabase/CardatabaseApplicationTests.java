0f4835bbfa6110cf5409cf12b1c6085abe4771b2
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 09:43:56 +0500

    Initial commit

---
+++ src/test/java/com/springbootdev/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdev.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8269e20a5589d89ea7c8fa17a02c2d1324edea7a
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 10:13:59 +0500

    Created Car Entity Class

---
+++ src/main/java/com/springbootdev/cardatabase/domain/Car.java
@@ -0,0 +1,91 @@
+package com.springbootdev.cardatabase.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String brand;
+	private String model;
+	private String color;
+	private String registerNumber;
+	
+	private int price;
+	
+	@Column(name="model_year")
+	private int year;
+	
+	public Car() {}
+	
+	
+
+	public Car(String brand, String model, String color, String registerNumber, int price, int year) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.price = price;
+		this.year = year;
+	}
+
+
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getRegisterNumber() {
+		return registerNumber;
+	}
+
+	public void setRegisterNumber(String registerNumber) {
+		this.registerNumber = registerNumber;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+}

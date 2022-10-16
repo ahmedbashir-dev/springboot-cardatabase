0b4275a9e8f1891cf74dc6c5d013c6a4f87e6c40
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 20:23:32 +0500

    Added JSONIgnore field to avoid loop in serialization process

--- src/main/java/com/springbootdev/cardatabase/domain/Owner.java
+++ src/main/java/com/springbootdev/cardatabase/domain/Owner.java
@@ -9,9 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Owner {
 
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private List<Car> cars;
 

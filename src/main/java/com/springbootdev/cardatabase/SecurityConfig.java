dec01e655c4bc20e0db70bc9a4078120aa6cd4b9
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 21:55:40 +0500

    Update SecurityConfig.java

--- src/main/java/com/springbootdev/cardatabase/SecurityConfig.java
+++ src/main/java/com/springbootdev/cardatabase/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.springbootdev.cardatabase;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("ahmed")
+				.password("admin123")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
 }

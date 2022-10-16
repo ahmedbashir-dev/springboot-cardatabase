6ea37250d3cba0e9fe6b27fc867f4a76749744e6
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 21:53:41 +0500

    Added Set Get methods

--- src/main/java/com/springbootdev/cardatabase/domain/User.java
+++ src/main/java/com/springbootdev/cardatabase/domain/User.java
@@ -1,5 +1,68 @@
 package com.springbootdev.cardatabase.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(nullable=false, updatable=false)
+	private Long id;
+	
+	@Column(nullable=false, unique=true)
+	private String username;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	@Column(nullable=false)
+	private String role;
+	
+	public User() {}
+	
+	
+
+	public User(String username, String password, String role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+	
+	
+	
 
 }

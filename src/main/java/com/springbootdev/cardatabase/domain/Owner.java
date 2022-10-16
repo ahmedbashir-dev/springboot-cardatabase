2b75e6d967433d6449f8679efa07b2f69a9dd52b
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 11:26:30 +0500

    Added One owner owns many cars relationship

--- src/main/java/com/springbootdev/cardatabase/domain/Owner.java
+++ src/main/java/com/springbootdev/cardatabase/domain/Owner.java
@@ -1,28 +1,42 @@
 package com.springbootdev.cardatabase.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Owner {
 
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+	private List<Car> cars;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long ownerId;
-	
+
 	private String firstname, lastname;
-	
-	public Owner() {}
-	
+
+	public Owner() {
+	}
+
 	public Owner(String firstname, String lastname) {
 		super();
 		this.firstname = firstname;
 		this.lastname = lastname;
 	}
-	
-	
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
 
 	public String getFirstname() {
 		return firstname;
@@ -40,5 +54,4 @@ public class Owner {
 		this.lastname = lastname;
 	}
 
-	
 }

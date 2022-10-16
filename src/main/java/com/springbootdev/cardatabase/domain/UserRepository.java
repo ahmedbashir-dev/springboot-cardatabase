acb5e5a67b30fbd1755f7b0fbd4de39145d459d4
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 22:36:56 +0500

    Disabled exporting as Rest Resource

--- src/main/java/com/springbootdev/cardatabase/domain/UserRepository.java
+++ src/main/java/com/springbootdev/cardatabase/domain/UserRepository.java
@@ -3,7 +3,9 @@ package com.springbootdev.cardatabase.domain;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported=false)
 public interface UserRepository extends CrudRepository<User,Long>{
 	public Optional<User> findByUsername(String username);
 }

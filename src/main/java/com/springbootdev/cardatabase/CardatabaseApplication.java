82ed948d2864c48000ef5f8ebc475e85ded07ac5
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 11:42:31 +0500

    Update CardatabaseApplication.java

--- src/main/java/com/springbootdev/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/springbootdev/cardatabase/CardatabaseApplication.java
@@ -1,4 +1,6 @@
 package com.springbootdev.cardatabase;
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springbootdev.cardatabase.domain.Car;
 import com.springbootdev.cardatabase.domain.CarRepository;
+import com.springbootdev.cardatabase.domain.Owner;
+import com.springbootdev.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
@@ -17,25 +21,31 @@ public class CardatabaseApplication implements CommandLineRunner {
 	@Autowired
 	private CarRepository carRepository;
 	
+	@Autowired
+	private OwnerRepository ownerRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		carRepository.save(new Car("Toyota", "Camry", "Indigo", "PAK-1123",2005,50000));
-		carRepository.save(new Car("Toyota", "Corolla", "Black", "LHR-2014",2012,13000));
-		carRepository.save(new Car("Toyota", "Avalon", "White", "LIS-2102",2015,80000));
-		carRepository.save(new Car("Toyota", "GR86", "Red", "PAK-0007",2022,800000));
-		carRepository.save(new Car("Honda", "City", "Green", "PAK-2223",2005,20000));
-		carRepository.save(new Car("Honda", "Civic", "Black", "PAK-2023",2009,21000));
-		carRepository.save(new Car("Honda", "Accord", "Dark Green", "PAK-5565",2002,50000));
-		carRepository.save(new Car("Suzuki", "WagonR", "White", "PAK-5454",2015,17000));
-		carRepository.save(new Car("Suzuki", "Swift", "Blue", "PAK-9045",2022,154500));
-		carRepository.save(new Car("Suzuki", "Alto", "White", "PAK-0212",2015,10000));
-		carRepository.save(new Car("Honda", "NOne", "Maroon", "PAK-2120",2011,50000));
-		carRepository.save(new Car("Toyota", "Camry", "Black", "PAK-1543",2009,50000));
+		Owner o1 = new Owner("Ahmed","Bashir");
+		Owner o2 = new Owner("Zeeshan", "Zulfiqar");
+		ownerRepository.saveAll(Arrays.asList(o1,o2));
+		
+		carRepository.save(new Car("Toyota", "Camry", "Indigo", "PAK-1123",2005,50000,o1));
+		carRepository.save(new Car("Toyota", "Corolla", "Black", "LHR-2014",2012,13000,o1));
+		carRepository.save(new Car("Toyota", "Avalon", "White", "LIS-2102",2015,80000,o1));
+		carRepository.save(new Car("Toyota", "GR86", "Red", "PAK-0007",2022,800000,o2));
+		carRepository.save(new Car("Honda", "City", "Green", "PAK-2223",2005,20000,o2));
+		carRepository.save(new Car("Honda", "Civic", "Black", "PAK-2023",2009,21000,o2));
+		carRepository.save(new Car("Honda", "Accord", "Dark Green", "PAK-5565",2002,50000,o1));
+		carRepository.save(new Car("Suzuki", "WagonR", "White", "PAK-5454",2015,17000,o1));
+		carRepository.save(new Car("Suzuki", "Swift", "Blue", "PAK-9045",2022,154500,o1));
+		carRepository.save(new Car("Suzuki", "Alto", "White", "PAK-0212",2015,10000,o2));
+		carRepository.save(new Car("Honda", "NOne", "Maroon", "PAK-2120",2011,50000,o1));
+		carRepository.save(new Car("Toyota", "Camry", "Black", "PAK-1543",2009,50000,o2));
 		
 		
 		for(Car c : carRepository.findAll()) {

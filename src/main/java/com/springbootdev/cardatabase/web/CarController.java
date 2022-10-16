9da7710f181c20f1f5e8363002142615f4726223
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-16 20:16:40 +0500

    Added RequestMapping for getCars

--- src/main/java/com/springbootdev/cardatabase/web/CarController.java
+++ src/main/java/com/springbootdev/cardatabase/web/CarController.java
@@ -1,8 +1,21 @@
 package com.springbootdev.cardatabase.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springbootdev.cardatabase.domain.Car;
+import com.springbootdev.cardatabase.domain.CarRepository;
 
 @RestController
 public class CarController {
 
+	@Autowired
+	private CarRepository carRepository;
+	
+	@RequestMapping(value="/cars", method = RequestMethod.GET)
+	public Iterable<Car> getCars(){
+		return carRepository.findAll();
+	}
 }

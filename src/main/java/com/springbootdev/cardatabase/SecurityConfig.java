4b6dbc9117a5a65eb67544738ca374649ea3ab6f
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-10-25 06:59:42 +0500

    Added configure method

--- src/main/java/com/springbootdev/cardatabase/SecurityConfig.java
+++ src/main/java/com/springbootdev/cardatabase/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.springbootdev.cardatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,7 +20,7 @@ import com.springbootdev.cardatabase.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
 	
@@ -33,6 +36,24 @@ public class SecurityConfig {
 //	protected void configure(HttpSecurity http) throws Exception{
 //		
 //	}
-//	
+//	]]
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/login")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+	}
+	
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception {
+	return authenticationManager();
+	}
 	
 }
